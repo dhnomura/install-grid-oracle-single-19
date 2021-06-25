50667e44205d01c8faad5da7a30281ba62179179
Diogo Hikaru Nomura <dhikaru@gmail.com>
2021-06-25 19:13:35 -0300

    Update create_directories.sh

--- create_directories.sh
+++ create_directories.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-declare -i group_counter=1001
-declare -i user_counter=1001
-
 function check_dir {
     DIR=$VAL
     if [ -d "$DIR" ]; then
         ### Take action if $DIR exists ###
-        chown oracle:oinstall $DIR
+        chown $RDBMS_GROUP:$PRODUCT_GROUP $DIR
         chmod 775 $DIR 
     else
         ###  Control will jump here if $DIR does NOT exists ###
         mkdir -p $DIR
-        chown oracle:oinstall $DIR 
+        chown $RDBMS_GROUP:$PRODUCT_GROUP $DIR 
         chmod 775 $DIR
     fi
 }
@@ -24,6 +21,11 @@ GRID_BASE=`cat parameters.json | jq '.operating_system.directories.grid_base'|se
 ORACLE_BASE=`cat parameters.json | jq '.operating_system.directories.oracle_base'|sed -e 's/^"//' -e 's/"$//'`
 GRID_HOME=`cat parameters.json | jq '.operating_system.directories.grid_home'|sed -e 's/^"//' -e 's/"$//'`
 ORACLE_HOME=`cat parameters.json | jq '.operating_system.directories.goracle_homerid_home'|sed -e 's/^"//' -e 's/"$//'`
+PRODUCT_GROUP=`cat parameters.json | jq '.operating_system.os_groups.product_group'|sed -e 's/^"//' -e 's/"$//'`
+RDBMS_GROUP=`cat parameters.json | jq '.operating_system.os_groups.rdbms_group'|sed -e 's/^"//' -e 's/"$//'`
+GRID_GROUP=`cat parameters.json | jq '.operating_system.os_groups.grid_group'|sed -e 's/^"//' -e 's/"$//'`
+GRID_OWNER=`cat parameters.json | jq '.operating_system.os_users.grid_owner'|sed -e 's/^"//' -e 's/"$//'`
+RDBMS_OWNER=`cat parameters.json | jq '.operating_system.os_users.database_owner'|sed -e 's/^"//' -e 's/"$//'`
 
 cat parameters.json \
     |jq '.operating_system.directories' \
