e7b54c743c98f1a62e8d80e3744c20c158d6dc3b
Diogo Hikaru Nomura <dhikaru@gmail.com>
2021-06-27 10:35:46 -0300

    Update create_grid_response_files.sh

--- create_grid_response_files.sh
+++ create_grid_response_files.sh
@@ -1,12 +1,32 @@
 
+#!/bin/bash
+
+ORACLE_BASE=`cat parameters.json | jq '.operating_system.directories.oracle_base'|sed -e 's/^"//' -e 's/"$//'`
+PRODUCT_GROUP=`cat parameters.json | jq '.operating_system.os_groups.product_group'|sed -e 's/^"//' -e 's/"$//'`
+ORAIVENTORY=`cat parameters.json | jq '.operating_system.directories.oraInventory'|sed -e 's/^"//' -e 's/"$//'`
+
+
+TFA_HOME=`cat parameters.json | jq '.operating_system.directories.tfa_home'|sed -e 's/^"//' -e 's/"$//'`
+GRID_BASE=`cat parameters.json | jq '.operating_system.directories.grid_base'|sed -e 's/^"//' -e 's/"$//'`
+GRID_HOME=`cat parameters.json | jq '.operating_system.directories.grid_home'|sed -e 's/^"//' -e 's/"$//'`
+ORACLE_HOME=`cat parameters.json | jq '.operating_system.directories.oracle_home'|sed -e 's/^"//' -e 's/"$//'`
+
+RDBMS_GROUP=`cat parameters.json | jq '.operating_system.os_groups.rdbms_group'|sed -e 's/^"//' -e 's/"$//'`
+GRID_GROUP=`cat parameters.json | jq '.operating_system.os_groups.grid_group'|sed -e 's/^"//' -e 's/"$//'`
+GRID_OWNER=`cat parameters.json | jq '.operating_system.os_users.grid_owner'|sed -e 's/^"//' -e 's/"$//'`
+RDBMS_OWNER=`cat parameters.json | jq '.operating_system.os_users.database_owner'|sed -e 's/^"//' -e 's/"$//'`
+ORACLE_SID=`cat parameters.json | jq '.oracle_rdbms.sid_name'|sed -e 's/^"//' -e 's/"$//'`
+
+GRID_HOME=`cat parameters.json | jq '.grid.directories.grid_home'|sed -e 's/^"//' -e 's/"$//'`
+
 echo " 
 oracle.install.responseFileVersion=/oracle/install/rspfmt_crsinstall_response_schema_v19.0.0
-INVENTORY_LOCATION=
+INVENTORY_LOCATION=$ORAIVENTORY
 oracle.install.option=CRS_SWONLY
-ORACLE_BASE=
-oracle.install.asm.OSDBA=
-oracle.install.asm.OSOPER=
-oracle.install.asm.OSASM=
+ORACLE_BASE=$ORACLE_BASE
+oracle.install.asm.OSDBA=$PRODUCT_GROUP
+oracle.install.asm.OSOPER=$PRODUCT_GROUP
+oracle.install.asm.OSASM=$PRODUCT_GROUP
 oracle.install.asm.SYSASMPassword=
 oracle.install.asm.diskGroup.name=
 oracle.install.asm.diskGroup.redundancy=
